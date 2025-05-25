853cb2d50ae1b3451532c48567826277fcdfd498
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-05-24 23:30:52 -0300

    att

--- testes/exercicio2.c
+++ testes/exercicio2.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main () {
-    
+    int N;
+    scanf("%d", &N);
+    char *texto = (char*)malloc(N*sizeof(char));
+    scanf(" %[^\n]", texto);
+    char palavra[2] = "oi";
+    int quant = 0;
+    int i = 0;
+    while (*(texto + i) != '\0') {
+        if (*(texto + i) == palavra) quant++;
+        i++;
+    }
+    printf("%d", quant);
 }
--- testes/exercicio8.c
+++ testes/exercicio8.c
@@ -3,16 +3,15 @@
 
 int main () {
     int matriz[3][5];
-    int vetor[3];
-    int maior;
+    int vetor[5];
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 5; j++) scanf("%d", &matriz[i][j]);
     }
+    for (int i = 0; i < 5; i++) vetor[i] = 0;
     for (int i = 0; i < 3; i++) {
-        vetor[i] = matriz[i][0];
         for (int j = 0; j < 5; j++) {
-            if (matriz[i][j] > vetor[i]) vetor[i] = matriz[i][j];
+            if (matriz[i][j]%2 == 0) vetor[j] += matriz[i][j];
         }
     }
-    for (int i = 0; i < 3; i++) printf("Maior da linha %d: %d!\n", i, vetor[i]);
+    for (int i = 0; i < 5; i++) printf("Soma dos pares da coluna %d: %d!\n", i, vetor[i]);
 }
