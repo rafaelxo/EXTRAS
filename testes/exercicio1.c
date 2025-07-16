0b7f39f65aa74827b79e3a06d589f41f1b05692a
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-07-16 01:31:46 -0300

    att

--- testes/exercicio1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char *nome = (char*)malloc(100*sizeof(char));
-    scanf(" %[^\n]", nome);
-    for (int i = strlen(nome) - 1; i >= 0; i--) printf("%c", *(nome + i));
-}
