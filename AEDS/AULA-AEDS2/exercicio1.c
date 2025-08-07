6075a0cf48e7c45f2d2011f99e28c0d6424b6bfa
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-07 11:31:58 -0300

    att

---
+++ AEDS/AULA-AEDS2/exercicio1.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int contMaiusc (char *str) {
+    int quant = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') quant++;
+    }
+    return quant;
+}
+
+int main () {
+    char *str = (char*)malloc(100*sizeof(char));
+    scanf(" %[^\n]", str);
+    while (str[0] != 'F' && str[1] != 'I' && str[2] != 'M' && str[3] != '\0') {
+        printf("%d\n", contMaiusc(str));
+        scanf(" %[^\n]", str);
+    }
+}
