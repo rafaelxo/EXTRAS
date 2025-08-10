9d959a661c7e18526dde34dcc1c556ea82dc9aa0
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-10 12:44:44 -0300

    att

---
+++ AEDS/EXERCÍCIOS/NIVELAMENTOb/exercicio1.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int comparar(int vet[], int N, int x) {
+    for (int i = 0; i < N; i++) {
+        if (vet[i] == x) return 1;
+    }
+}
+
+int main () {
+    int N; scanf("%d", &N);
+    int vet[N]; for (int i = 0; i < N; i++) scanf("%d", &vet[i]);
+    int x; scanf("%d", &x);
+    int comp = comparar(vet, N, x);
+    if (comp == 1) printf("SIM\n");
+    else printf("NAO\n");
+}
