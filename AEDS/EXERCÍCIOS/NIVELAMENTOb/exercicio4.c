a1411307a0a1d90c286b87bde0e1983dd75c97d4
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-10 14:21:38 -0300

    att

--- AEDS/EXERCÍCIOS/NIVELAMENTOb/exercicio4.c
+++ AEDS/EXERCÍCIOS/NIVELAMENTOb/exercicio4.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void elementos(int vet[], int N) {
-    int maior = vet[0];
-    int menor = vet[0];
-    for (int i = 0; i < N; i++) {
-        if (vet[i] > maior) maior = vet[i];
-        if (vet[i] < menor) menor = vet[i];
+void elementosOtim(int vet[], int N) {
+    int maior = vet[0]; int menor = vet[0];
+    for (int i = 1; i < N - 1; i += 2) {
+        if (vet[i] > vet[i + 1]) {
+            if (vet[i] > maior) maior = vet[i];
+            if (vet[i + 1] < menor) menor = vet[i + 1];
+        } else {
+            if (vet[i + 1] > maior) maior = vet[i + 1];
+            if (vet[i] < menor) menor = vet[i];
+        }
     }
     printf("Menor: %d\nMaior: %d\n", menor, maior);
 }
@@ -14,5 +18,5 @@ void elementos(int vet[], int N) {
 int main() {
     int N; scanf("%d", &N);
     int vet[N]; for (int i = 0; i < N; i++) scanf("%d", &vet[i]);
-    elementos(vet, N);
+    elementosOtim(vet, N);
 }
