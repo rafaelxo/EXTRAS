f27a2f5b5393ad932f9a9a5beedbe477f8b44759
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-11 17:50:32 -0300

    att

--- AEDS/EXERCÍCIOS/NIVELAMENTO3/exercicio6.java
+++ AEDS/EXERCÍCIOS/NIVELAMENTO3/exercicio6.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class exercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        double media = 0;
+        int n = sc.nextInt(); double media = 0;
         int i = 0;
         while (i < n) {
             int num = sc.nextInt();
