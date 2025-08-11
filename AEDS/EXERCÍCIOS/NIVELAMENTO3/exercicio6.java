d427c28459fe878d73dc614dafd3eb78a5396d5a
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-11 17:42:18 -0300

    att

--- AEDS/EXERCÍCIOS/NIVELAMENTO3/exercicio6.java
+++ AEDS/EXERCÍCIOS/NIVELAMENTO3/exercicio6.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-public class exercicio5 {
+public class exercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        double media = 0;
         int i = 0;
         while (i < n) {
             int num = sc.nextInt();
-            if (num % 2 == 0) System.out.println("Par!");
-            else System.out.println("Ímpar");
+            media += num;
             i++;
         }
+        media /= n;
+        System.out.print("Média: " + media);
         sc.close();
     }
 }
