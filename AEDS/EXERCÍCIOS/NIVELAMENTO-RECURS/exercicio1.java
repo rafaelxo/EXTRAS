820b99305e1079e5b56a55224bbe4c1fa58782a1
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-15 23:49:14 -0300

    att

--- AEDS/EXERCÍCIOS/NIVELAMENTO-RECURS/exercicio1.java
+++ AEDS/EXERCÍCIOS/NIVELAMENTO-RECURS/exercicio1.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
 public class exercicio1 {
-    public static int contVogal (String str) {
+    public static int contVogal(String str) {
         if (str.length() == 0) return 0;
-        else if (str.charAt(0) == 'A' || str.charAt(0) == 'E' || str.charAt(0) == 'I' || str.charAt(0) == 'O' || str.charAt(0) == 'U') return 1 + contVogal(str.substring(1));
+        char c = str.charAt(0);
+        if (c >= 'a' && c <= 'z') c = (char) (c - 32);
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') return 1 + contVogal(str.substring(1));
         else return contVogal(str.substring(1));
     }
-    public static int contConsoante (String str) {
+
+    public static int contConsoante(String str) {
         if (str.length() == 0) return 0;
-        else if ((str.charAt(0) >= 'A' && str.charAt(0) <= 'Z') && !(str.charAt(0) == 'A' || str.charAt(0) == 'E' || str.charAt(0) == 'I' || str.charAt(0) == 'O' || str.charAt(0) == 'U')) return 1 + contConsoante(str.substring(1));
+        char c = str.charAt(0);
+        if (c >= 'a' && c <= 'z') c = (char) (c - 32);
+        if ((c >= 'A' && c <= 'Z') && !(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')) return 1 + contConsoante(str.substring(1));
         else return contConsoante(str.substring(1));
     }
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         System.out.println("Vogais: " + contVogal(str));
