f09241b7c39d5d133830d3365a09c85305e4e6c7
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-28 09:19:11 -0300

    att

---
+++ AEDS/AULA-AEDS2/espelho.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class espelho {
+    public static Scanner sc = new Scanner (System.in);
+    public static void imprimeInvertido (int n) {
+        if (n < 10) System.out.print(n);
+        else {
+            System.out.print(n%10);
+            imprimeInvertido(n/10);
+        }
+    }
+    public static void espelhar (int n1, int n2) {
+        for (int i = n1; i <= n2; i++) System.out.print(i);
+        for (int i = n2; i >= n1; i--) imprimeInvertido(i);
+    }
+    public static void main (String[] args) {
+        int n1 = sc.nextInt(); int n2 = sc.nextInt();
+        while (n1 != -1) {
+            espelhar(n1, n2);
+            System.out.println();
+            n1 = sc.nextInt(); n2 = sc.nextInt();
+        }
+    }
+}
