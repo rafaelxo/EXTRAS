4654e74517162e0055fe9e6156f3e5cb38001d4c
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-28 11:34:58 -0300

    att

--- AEDS/AULA-AEDS2/combinador.c
+++ AEDS/AULA-AEDS2/combinador.c
@@ -8,19 +8,18 @@ int length (char str[]) {
 }
 
 void combinacao (char str1[], char str2[]) {
-    int maior = length(str1);
-    if (length(str2) > maior) maior = length(str2);
+    int len1 = length(str1), len2 = length(str2);
+    int maior = len1 > len2 ? len1 : len2;
     int i = 0;
     while (i < maior) {
-        if (i < length(str1)) printf ("%c", str1[i]);
-        if (i < length(str2)) printf("%c", str2[i]);
+        if (i < len1) printf ("%c", str1[i]);
+        if (i < len2) printf("%c", str2[i]);
         i++;
     }
 }
 
 int main () {
     char str1[100], str2[100];
-    scanf("%s %s", str1, str2);
     while (scanf("%s %s", str1, str2) != EOF) {
         combinacao(str1, str2);
         printf("\n");
