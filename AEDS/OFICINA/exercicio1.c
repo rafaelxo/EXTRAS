982c32882773076f3029ec6d3de49d2837cc7191
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-04 14:13:27 -0300

    att

---
+++ AEDS/OFICINA/exercicio1.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+
+#define MAX_EMERGENCIA 5
+#define MAX_PERIODICA 5
+#define MAX_BACKGROUND 30
+
+typedef struct {
+    int id;
+    int prioridade;
+} Tarefa;
+
+typedef struct {
+    Tarefa pilha[MAX_EMERGENCIA];
+    int n;
+} PilhaEmergencia;
+
+typedef struct {
+    Tarefa fila[MAX_PERIODICA];
+    int primeiro;
+    int ultimo;
+} FilaPeriodica;
+
+typedef struct {
+    Tarefa lista[MAX_BACKGROUND];
+    int n;
+} ListaBackground;
+
+Tarefa processarTarefa(PilhaEmergencia *p, FilaPeriodica *f, ListaBackground *l) {
+    // TO DO: IMPLEMENTAR
+    // desempilhar uma tarefa da pilha (se houver)
+    // OU desinfileirar uma tarefa da fila (se houver)
+    // OU retirar a tarefa mais prioritária da lista
+}
+
+void promoverTarefa(PilhaEmergencia *p, ListaBackground *l, int id) {
+    // TO DO: IMPLEMENTAR
+    // passar a tarefa de identificador id da lista para a pilha
+}
+
+void empilharEmergencia(PilhaEmergencia *p, Tarefa t) {
+    // TO DO: IMPLEMENTAR
+    // empilhar uma tarefa na pilha
+}
+
+Tarefa desempilharEmergencia(PilhaEmergencia *p) {
+    // TO DO: IMPLEMENTAR
+    // desempilhar uma tarefa da pilha
+}
+
+void enfileirarPeriodica(FilaPeriodica *f, Tarefa t) {
+    // TO DO: IMPLEMENTAR
+    // enfilierar um elemento na fila
+}
+
+Tarefa desenfileirarPeriodica(FilaPeriodica *f) {
+    // TO DO: IMPLEMENTAR
+    // desinfileirar uma tarefa da fila
+}
+
+void inserirBackground(ListaBackground *l, Tarefa t) {
+    // TO DO: IMPLEMENTAR
+    // inserir uma tarefa na lista
+}
+
+Tarefa removerBackground(ListaBackground *l) {
+    // TO DO: IMPLEMENTAR
+    // remover a tarefa mais prioritaria da lista
+}
+
+void imprimirEstruturas(PilhaEmergencia *p, FilaPeriodica *f, ListaBackground *l) {
+    // TO DO: IMPLEMENTAR
+    // printar a pilha, lista e fila
+}
+
+void criarTarefa(PilhaEmergencia *p, FilaPeriodica *f, ListaBackground *l) {
+    Tarefa tarefa;
+    printf("ID: "); scanf("%d", &tarefa.id);
+    printf("Prioridade: "); scanf("%d", &tarefa.prioridade);
+    printf("1 - Inserir na Pilha\n2 - Inserir na Fila\n3 - Inserir na lista\n");
+    int op; scanf("%d", &op);
+    switch (op) {
+        case 1: empilharEmergencia(p, tarefa); break;
+        case 2: enfileirarPeriodica(f, tarefa); break;
+        case 3: inserirBackground(l, tarefa); break;
+        default: printf("Opcao invalida!\n"); return;
+    }
+}
+
+int main() {
+    PilhaEmergencia pilha;
+    pilha.n = 0;
+    FilaPeriodica fila;
+    fila.primeiro = 0;
+    fila.ultimo = 0;
+    ListaBackground lista;
+    lista.n = 0;
+
+    int op;
+    do {
+        printf("\nMENU\n");
+        printf("\n1 - Criar Tarefa");
+        printf("\n2 - Processar Tarefa");
+        printf("\n3 - Promover Tarefa");
+        printf("\n0 - Sair\n");
+        printf("Entre com a opcao: ");
+        scanf("%d", &op);
+        switch (op) {
+            case 1: criarTarefa(&pilha, &fila, &lista); break;
+            case 2:
+                Tarefa processada = processarTarefa(&pilha, &fila, &lista);
+                printf("Processei tarefa %d\n", processada.id);
+                break;
+            case 3:
+                printf("Qual o ID da tarefa a ser promovida?\n");
+                int id; scanf("%d", &id);
+                promoverTarefa(&pilha, &lista, id);
+                break;
+            case 0: printf("Saindo...\n"); break;
+            default: printf("Opcao invalida!\n"); break;
+        }
+        imprimirEstruturas(&pilha, &fila, &lista);
+    } while (op != 0);
+    return 0;
+}
