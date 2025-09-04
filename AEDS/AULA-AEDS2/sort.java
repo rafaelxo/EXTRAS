b8db5b075700ce0164ead208ee89de9849007645
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-04 08:34:41 -0300

    att

---
+++ AEDS/AULA-AEDS2/sort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sort {
+    public static Scanner sc = new Scanner (System.in);
+    public static void main (String[] args) {
+        int n = sc.nextInt(); int m = sc.nextInt();
+        int[] nums = new int[n]; for (int i  = 0; i < n; i++) nums[i] = sc.nextInt();
+
+        while (n != 0 && m != 0) {
+
+        }
+        System.out.println("0 0");
+    }
+}
