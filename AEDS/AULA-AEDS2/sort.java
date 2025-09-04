82e9d2b2589f73f74afb2918e8417f5e0a4d1c14
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-04 11:18:45 -0300

    att

--- AEDS/AULA-AEDS2/sort.java
+++ AEDS/AULA-AEDS2/sort.java
@@ -2,12 +2,35 @@ import java.util.*;
 
 public class sort {
     public static Scanner sc = new Scanner (System.in);
+    public static void swap (int[] v, int i, int j) {
+        int aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+    }
+    public static void selecao (int[] v) {
+        for (int i = 0; i < v.length - 1; i++) {
+            int menor = i;
+            for (int j = i + 1; j < v.length; j++) {
+                if (v[j] < v[menor]) menor = j;
+            }
+            swap(v, i, menor);
+        }
+    }
+    public static void esquematizacao (int[] v, int n, int m) {
+        for (int i = n - 1; i >= 0; i++) {
+            if (v[i] % m == 0 && v[i] % 2 != 0) System.out.println(v[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (v[i] % m != 0 && v[i] % 2 == 0) System.out.println(v[i]);
+        }
+    }
     public static void main (String[] args) {
         int n = sc.nextInt(); int m = sc.nextInt();
         while (n != 0 && m != 0) {
             int[] nums = new int[n];
             for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
-            
+            selecao(nums);
+            esquematizacao(nums, n, m);
         }
         System.out.println("0 0");
     }
