fc5ace18c42b46d0c4cf3fa87a46206fddd8c8ec
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-04 11:05:46 -0300

    att

--- AEDS/AULA-AEDS2/sort.java
+++ AEDS/AULA-AEDS2/sort.java
@@ -4,10 +4,10 @@ public class sort {
     public static Scanner sc = new Scanner (System.in);
     public static void main (String[] args) {
         int n = sc.nextInt(); int m = sc.nextInt();
-        int[] nums = new int[n]; for (int i  = 0; i < n; i++) nums[i] = sc.nextInt();
-
         while (n != 0 && m != 0) {
-
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+            
         }
         System.out.println("0 0");
     }
