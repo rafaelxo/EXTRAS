bb14f07ae0f68269e8dbc12c1fc59b98023d2988
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-08 19:05:38 -0300

    att

---
+++ TESTES/insercao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void insercao (int vet[], int n) {
+    for (int i = 1; i < n; i++) {
+        int aux = vet[i];
+        int j = i - 1;
+        while ((j >= 0) && (vet[j] > aux)) {
+            vet[j + 1] = vet[j];
+            j--;
+        }
+        vet[j + 1] = aux;
+    }
+}
---
+++ TESTES/selecao.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void swap (int vet[], int i, int j) {
+    int aux = vet[i];
+    vet[i] = vet[j];
+    vet[j] = aux;
+}
+
+void selecao (int vet[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int menor = i;
+        for (int j = i + 1; j < n; j++) {
+            if (vet[j] < vet[menor]) menor = j;
+        }
+        swap(vet, i, menor);
+    }
+}
