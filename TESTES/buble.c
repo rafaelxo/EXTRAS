b7e94a682ce15ddf39bb1d50d4e86d74422df93e
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-08 19:08:46 -0300

    att

--- TESTES/buble.c
+++ TESTES/buble.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+void swap (int vet[], int i, int j) {
+    int aux = vet[i];
+    vet[i] = vet[j];
+    vet[j] = aux;
+}
+
+void bolha (int vet[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (vet[j] > vet[j + 1]) swap (vet, j, j + 1);
+        }
+    }
+}
