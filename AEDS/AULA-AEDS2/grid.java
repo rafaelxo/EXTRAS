d46af0b567665eda9f50eb7bf494748c563062cf
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-10 20:09:35 -0300

    att

---
+++ AEDS/AULA-AEDS2/grid.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class grid {
+    public static Scanner sc = new Scanner (System.in);
+
+    public static int ultrapassagens (int[] larg, int[] cheg) {
+        int quant = 0;
+
+        return quant;
+    }
+
+    public static void main (String[] args) {
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int[] largada = new int[n]; for (int i = 0; i < n; i++) largada[i] = sc.nextInt();
+            int[] chegada = new int[n]; for (int i = 0; i < n; i++) chegada[i] = sc.nextInt();
+
+            System.out.println();
+        }
+    }
+}
