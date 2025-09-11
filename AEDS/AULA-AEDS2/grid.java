ad06a261159b596bd6666d251bedf57fc8bbc270
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-11 09:19:21 -0300

    att

--- AEDS/AULA-AEDS2/grid.java
+++ AEDS/AULA-AEDS2/grid.java
@@ -3,10 +3,27 @@ import java.util.*;
 public class grid {
     public static Scanner sc = new Scanner (System.in);
 
-    public static int ultrapassagens (int[] larg, int[] cheg) {
-        int quant = 0;
+    public static int insercao (int[] vet, int n) {
+        int mov = 0;
+        for (int i = 1; i < n; i++) {
+            int aux = vet[i];
+            int j = i - 1;
+            while (j >= 0 && vet[j] > aux) {
+                vet[j + 1] = vet[j];
+                j--;
+                mov++;
+            }
+            vet[j + 1] = aux;
+            mov += 2;
+        }
+        return mov;
+    }
 
-        return quant;
+    public static int ultrapassagens (int[] largada, int[] chegada, int n) {
+        int larg = insercao(largada, n);
+        int cheg = insercao(chegada, n);
+        if (larg > cheg) return larg - cheg;
+        else return cheg - larg;
     }
 
     public static void main (String[] args) {
@@ -14,8 +31,7 @@ public class grid {
             int n = sc.nextInt();
             int[] largada = new int[n]; for (int i = 0; i < n; i++) largada[i] = sc.nextInt();
             int[] chegada = new int[n]; for (int i = 0; i < n; i++) chegada[i] = sc.nextInt();
-
-            System.out.println();
+            System.out.println((ultrapassagens(largada, chegada, n)));
         }
     }
 }
