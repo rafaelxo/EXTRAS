a88790b07fad1b8b1132d7d3299d2d36302e286c
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-11 09:46:38 -0300

    att

--- AEDS/AULA-AEDS2/sort.java
+++ AEDS/AULA-AEDS2/sort.java
@@ -2,27 +2,25 @@ import java.util.*;
 
 public class sort {
     public static Scanner sc = new Scanner (System.in);
-    public static void swap (int[] v, int i, int j) {
-        int aux = v[i];
-        v[i] = v[j];
-        v[j] = aux;
-    }
-    public static void selecao (int[] v) {
-        for (int i = 0; i < v.length - 1; i++) {
-            int menor = i;
-            for (int j = i + 1; j < v.length; j++) {
-                if (v[j] < v[menor]) menor = j;
+    public static void insercao (int[] vet, int n) {
+        for (int i = 1; i < n; i++) {
+            int aux = vet[i];
+            int j = i - 1;
+            while (j >= 0 && vet[j] > aux) {
+                vet[j + 1] = vet[j];
+                j--;
             }
-            swap(v, i, menor);
+            vet[j + 1] = aux;
         }
     }
-    public static void esquematizacao (int[] v, int n, int m) {
+    public static void esquematizacao (int[] vet, int n, int m) {
+        insercao(vet, n);
         for (int mod = -m + 1; mod < m; mod++) {
             for (int i = n - 1; i >= 0; i--) {
-                if (v[i] % m == mod && v[i] % 2 != 0) System.out.println(v[i]);
+                if (vet[i] % m == mod && vet[i] % 2 != 0) System.out.println(vet[i]);
             }
             for (int i = 0; i < n; i++) {
-                if (v[i] % m == mod && v[i] % 2 == 0) System.out.println(v[i]);
+                if (vet[i] % m == mod && vet[i] % 2 == 0) System.out.println(vet[i]);
             }
         }
     }
@@ -32,7 +30,7 @@ public class sort {
             System.out.println(n + " " + m);
             int[] nums = new int[n];
             for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
-            selecao(nums); esquematizacao(nums, n, m);
+            esquematizacao(nums, n, m);
             n = sc.nextInt(); m = sc.nextInt();
         }
         System.out.println(n + " " + m);
