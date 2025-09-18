6370b2498db0a463f1aae25894881b254d8bf618
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-18 09:29:35 -0300

    att

--- AEDS/PRÁTICA/diretores.java
+++ AEDS/PRÁTICA/diretores.java
@@ -5,7 +5,9 @@ public class diretores {
     public static void main (String[] args) {
         while (sc.hasNextInt()) {
             int n = sc.nextInt(); int k = sc.nextInt();
-            if ((k / n) >= 1) System.out.println(k / n);
+            int t = (k - (n - 1)) / n;
+            if (t < 1) t = 1;
+            System.out.println(t);
         }
     }
 }
--- AEDS/PRÁTICA/estatura.c
+++ AEDS/PRÁTICA/estatura.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main () {
-    
+    int n, h;
+    scanf("%d%d", &n, &h);
+    int brinq[n];
+    int cont = 0;
+    for (int i = 0; i < n; i++) scanf("%d", &brinq[i]);
+    for (int i = 0; i < n; i++) {
+        if (h >= brinq[i]) cont++;
+    }
+    printf("%d\n", cont);
 }
