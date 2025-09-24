56b8d436e37c1ffaf11c5a693a0b04462a0c94ef
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-24 18:15:26 -0300

    att

--- AEDS/PRÁTICA/quicksort.java
+++ AEDS/PRÁTICA/quicksort.java
@@ -5,35 +5,163 @@ public class quicksort {
     public static void QuickSortFirstPivot (int[] vet, int left, int right) {
         
     }
-    public static void QuickSortLastPivot(int[] vet, int left, int right) {
+    public static void QuickSortLastPivot (int[] vet, int left, int right) {
 
     }
-    public static void QuickSortRandomPivot(int[] vet, int left, int right) {
+    public static void QuickSortRandomPivot (int[] vet, int left, int right) {
 
     }
-    public static void QuickSortMedianofThree(int[] vet, int left, int right) {
+    public static void QuickSortMedianofThree (int[] vet, int left, int right) {
 
     }
     public static void main (String[] args) {
-        int[] dez = new int[10]; for (int i = 0; i < 10; i++) dez[i] = ger.nextInt(11);
-        System.out.println("Array com 10 inteiros:");
+        int[] dez = new int[10];
+        int[] cem = new int[100];
+        int[] mil = new int[1000];
+
+        System.out.println("QUICKSORT COM PIVO NO PRIMEIRO ELEMENTO:");
+
+        for (int i = 0; i < 10; i++) dez[i] = ger.nextInt(11);
+        System.out.println("Array com 10 inteiros desordenado:");
         System.out.println("[ ");
         for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
         System.out.print("]");
-        int[] cem = new int[100]; for (int i = 0; i < 100; i++) cem[i] = ger.nextInt(101);
-        System.out.println("Array com 100 inteiros:");
-        System.out.println("[ "); for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+
+        for (int i = 0; i < 100; i++) cem[i] = ger.nextInt(101);
+        System.out.println("Array com 100 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
         System.out.print("]");
-        int[] mil = new int[1000]; for (int i = 0; i < 1000; i++) mil[i] = ger.nextInt(1001);
-        System.out.println("Array com 1000 inteiros:");
-        System.out.println("[ "); for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+
+        for (int i = 0; i < 1000; i++) mil[i] = ger.nextInt(1001);
+        System.out.println("Array com 1000 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
         System.out.print("]");
-        System.out.println("QuickSort com pivo no primeiro elemento:");
 
-        System.out.println("QuickSort com pivo no ultimo elemento:");
+        QuickSortFirstPivot(dez, 0, dez.length); QuickSortFirstPivot(cem, 0, cem.length); QuickSortFirstPivot(mil, 0, mil.length);
 
-        System.out.println("QuickSort com pivo aleatorio:");
+        System.out.println("Array com 10 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
 
-        System.out.println("QuickSort com pivo de mediana de tres:");
+        System.out.println("Array com 100 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 1000 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]");
+
+        /* System.out.println("QUICKSORT COM PIVO NO ULTIMO ELEMENTO:");
+
+        System.out.println("Array com 10 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 100 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 1000 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]");
+
+        QuickSortLastPivot(dez, 0, dez.length); QuickSortLastPivot(cem, 0, cem.length); QuickSortLastPivot(mil, 0, mil.length);
+
+        System.out.println("Array com 10 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 100 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 1000 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]"); */
+
+        /* System.out.println("QUICKSORT COM PIVO ALEATORIO:");
+
+        for (int i = 0; i < 10; i++) dez[i] = ger.nextInt(11);
+        System.out.println("Array com 10 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
+
+        for (int i = 0; i < 100; i++) cem[i] = ger.nextInt(101);
+        System.out.println("Array com 100 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        for (int i = 0; i < 1000; i++) mil[i] = ger.nextInt(1001);
+        System.out.println("Array com 1000 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]");
+
+        QuickSortLastPivot(dez, 0, dez.length); QuickSortLastPivot(cem, 0, cem.length); QuickSortLastPivot(mil, 0, mil.length);
+
+        System.out.println("Array com 10 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 100 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 1000 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]"); */
+
+        /* System.out.println("QUICKSORT COM PIVO DE MEDIANA DE TRES:");
+
+        for (int i = 0; i < 10; i++) dez[i] = ger.nextInt(11);
+        System.out.println("Array com 10 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
+
+        for (int i = 0; i < 100; i++) cem[i] = ger.nextInt(101);
+        System.out.println("Array com 100 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        for (int i = 0; i < 1000; i++) mil[i] = ger.nextInt(1001);
+        System.out.println("Array com 1000 inteiros desordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]");
+
+        QuickSortMedianofThree(dez, 0, dez.length); QuickSortMedianofThree(cem, 0, cem.length); QuickSortMedianofThree(mil, 0, mil.length);
+
+        System.out.println("Array com 10 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 10; i++) System.out.print(dez[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 100 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 100; i++) System.out.print(cem[i] + " ");
+        System.out.print("]");
+
+        System.out.println("Array com 1000 inteiros ordenado:");
+        System.out.println("[ ");
+        for (int i = 0; i < 1000; i++) System.out.print(mil[i] + " ");
+        System.out.print("]"); */
     }
 }
