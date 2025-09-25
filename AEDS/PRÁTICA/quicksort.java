23befbea50964f85e914a33e75499fca738510e2
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-24 21:18:31 -0300

    att

--- AEDS/PRÁTICA/quicksort.java
+++ AEDS/PRÁTICA/quicksort.java
@@ -13,20 +13,79 @@ public class quicksort {
         System.out.println("]");
     }
 
-    public static void QuickSortFirstPivot (int[] vet, int left, int right) {
-
+    public static void swap (int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 
-    public static void QuickSortLastPivot (int[] vet, int left, int right) {
-
+    public static void QuickSortFirstPivot (int[] array, int left, int right) {
+        int i = left, j = right;
+        int pivo = array[left];
+        while (i <= j) {
+            while (array[i] < pivo) i++;
+            while (array[j] > pivo) j--;
+            if (i <= j) {
+                swap(array, i, j);
+                i++; j--;
+            }
+        }
+        if (left < j) QuickSortFirstPivot(array, left, j);
+        if (i < right) QuickSortFirstPivot(array, i, right);
     }
 
-    public static void QuickSortRandomPivot (int[] vet, int left, int right) {
-
+    public static void QuickSortLastPivot (int[] array, int left, int right) {
+        int i = left, j = right;
+        int pivo = array[right];
+        while (i <= j) {
+            while (array[i] < pivo) i++;
+            while (array[j] > pivo) j--;
+            if (i <= j) {
+                swap(array, i, j);
+                i++; j--;
+            }
+        }
+        if (left < j) QuickSortLastPivot(array, left, j);
+        if (i < right) QuickSortLastPivot(array, i, right);
     }
 
-    public static void QuickSortMedianofThree (int[] vet, int left, int right) {
+    public static void QuickSortRandomPivot (int[] array, int left, int right) {
+        int i = left, j = right;
+        int p = left + ger.nextInt(right - left + 1);
+        int pivo = array[p];
+        while (i <= j) {
+            while (array[i] < pivo) i++;
+            while (array[j] > pivo) j--;
+            if (i <= j) {
+                swap(array, i, j);
+                i++; j--;
+            }
+        }
+        if (left < j) QuickSortRandomPivot(array, left, j);
+        if (i < right) QuickSortRandomPivot(array, i, right);
+    }
 
+    public static int MedianOfThree (int[] array, int left, int right) {
+        int meio = (left + right) / 2;
+        if (array[left] > array[meio]) swap(array, left, meio);
+        if (array[left] > array[right]) swap(array, left, right);
+        if (array[meio] > array[right]) swap(array, meio, right);
+        return array[meio];
+    }
+
+    public static void QuickSortMedianOfThree (int[] array, int left, int right) {
+        int i = left, j = right;
+        int pivo = MedianOfThree(array, left, right);
+        while (i <= j) {
+            while (array[i] < pivo) i++;
+            while (array[j] > pivo) j--;
+            if (i <= j) {
+                swap(array, i, j);
+                i++; j--;
+            }
+        }
+        if (left < j) QuickSortMedianOfThree(array, left, j);
+        if (i < right) QuickSortMedianOfThree(array, i, right);
     }
 
     public static void main (String[] args) {
@@ -70,7 +129,7 @@ public class quicksort {
         imprimirArray("Array com 100 inteiros desordenado:", cem);
         imprimirArray("Array com 1000 inteiros desordenado:", mil);
 
-        QuickSortLastPivot(dez, 0, dez.length - 1); QuickSortLastPivot(cem, 0, cem.length - 1); QuickSortLastPivot(mil, 0, mil.length - 1);
+        QuickSortRandomPivot(dez, 0, dez.length - 1); QuickSortRandomPivot(cem, 0, cem.length - 1); QuickSortRandomPivot(mil, 0, mil.length - 1);
 
         imprimirArray("Array com 10 inteiros ordenado:", dez);
         imprimirArray("Array com 100 inteiros ordenado:", cem);
@@ -84,7 +143,7 @@ public class quicksort {
         imprimirArray("Array com 100 inteiros desordenado:", cem);
         imprimirArray("Array com 1000 inteiros desordenado:", mil);
 
-        QuickSortMedianofThree(dez, 0, dez.length - 1); QuickSortMedianofThree(cem, 0, cem.length - 1); QuickSortMedianofThree(mil, 0, mil.length - 1);
+        QuickSortMedianOfThree(dez, 0, dez.length - 1); QuickSortMedianOfThree(cem, 0, cem.length - 1); QuickSortMedianOfThree(mil, 0, mil.length - 1);
 
         imprimirArray("Array com 10 inteiros ordenado:", dez);
         imprimirArray("Array com 100 inteiros ordenado:", cem);
