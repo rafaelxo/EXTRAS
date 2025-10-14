51a2f3dbf6e350dc39c13a9a09c0419fa098bd7c
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-10-14 17:41:35 -0300

    att

--- AEDS/BASES/Árvore/arvoreBin.java
+++ AEDS/BASES/Árvore/arvoreBin.java
@@ -18,7 +18,10 @@ public class arvoreBin {
         public Arvore () {
             this.raiz = null;
         }
-        void inserir (int x) { raiz = inserir (x, raiz); }
+
+        public void inserir (int x) {
+            raiz = inserir (x, raiz);
+        }
         private No inserir (int x, No i) {
             if (i == null) i = new No(x);
             else if (x < i.elemento) i.esq = inserir(x, i.esq);
@@ -26,7 +29,8 @@ public class arvoreBin {
             else throw new RuntimeException ("Erro!");
             return i;
         }
-        void inserirPai (int x) {
+
+        public void inserirPai (int x) {
             if (raiz == null) raiz = new No (x);
             else if (x < raiz.elemento) raiz.esq = inserir(x, raiz.esq);
             else if (x > raiz.elemento) raiz.dir = inserir(x, raiz.dir);
@@ -41,20 +45,73 @@ public class arvoreBin {
             else if (x > i.elemento) inserirPai(x, i.dir, i);
             else throw new RuntimeException ("Erro!");
         }
-        boolean pesquisar (int x) {
+
+        public boolean pesquisar (int x) {
+            return pesquisar(x, raiz);
+        }
+        private boolean pesquisar (int x, No i) {
+            boolean resp;
+            if (i == null) resp = false;
+            else if (x == i.elemento) resp = true;
+            else if (x < i.elemento) resp = pesquisar(x, i.esq);
+            else resp = pesquisar(x, i.dir);
+            return resp;
+        }
+
+        public void caminharCentral (No i) {
+            if (i != null) {
+                caminharCentral(i.esq);
+                System.out.print(i.elemento + " ");
+                caminharCentral(i.dir);
+            }
+        }
+
+        public void caminharPre (No i) {
+            if (i != null) {
+                System.out.print(i.elemento + " ");
+                caminharPre(i.esq);
+                caminharPre(i.dir);
+            }
+        }
+
+        public void caminharPos (No i) {
+            if (i != null) {
+                caminharPos(i.esq);
+                caminharPos(i.dir);
+                System.out.print(i.elemento + " ");
+            }
+        }
+
+        public void remover (int x) {
 
         }
-        void caminharCentral () {
 
+        public int getAltura () {
+            int h = 0, j =0;
+            for (No i = raiz; i != null; i = i.esq) h++;
+            for (No i = raiz; i != null; i = i.dir) j++;
+            return (h > j) ? h : j;
         }
-        void caminharPre () {
 
+        public int getAlturaRec (No i) {
+            if (i == null) return -1;
+            else {
+                int altEsq = getAlturaRec(i.esq);
+                int altDir = getAlturaRec(i.dir);
+                return (altEsq > altDir) ? altEsq + 1 : altDir + 1;
+            }
         }
-        void caminharPos () {
 
+        public int somar () {
+            int soma = 0;
+            for (No i = raiz; i != null; i = i.esq) soma += i.elemento;
+            for (No i = raiz; i != null; i = i.dir) soma += i.elemento;
+            return soma;
         }
-        void remover (int x) {
 
+        public int somarRec (No i) {
+            if (i == null) return 0;
+            else return i.elemento + somarRec(i.esq) + somarRec(i.dir);
         }
     }
 }
