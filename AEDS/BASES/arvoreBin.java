a70ce467d24e7c64a31f6775a0d1a859a5d499b6
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-10-14 00:03:36 -0300

    att

--- AEDS/BASES/arvoreBin.java
+++ AEDS/BASES/arvoreBin.java
@@ -18,11 +18,29 @@ public class arvoreBin {
         public Arvore () {
             this.raiz = null;
         }
-        void inserir (int x) {
-
+        void inserir (int x) { raiz = inserir (x, raiz); }
+        private No inserir (int x, No i) {
+            if (i == null) i = new No(x);
+            else if (x < i.elemento) i.esq = inserir(x, i.esq);
+            else if (x > i.elemento) i.dir = inserir(x, i.dir);
+            else throw new RuntimeException ("Erro!");
+            return i;
         }
         void inserirPai (int x) {
-
+            if (raiz == null) raiz = new No (x);
+            else if (x < raiz.elemento) raiz.esq = inserir(x, raiz.esq);
+            else if (x > raiz.elemento) raiz.dir = inserir(x, raiz.dir);
+            else throw new RuntimeException ("Erro!");
+        }
+        private void inserirPai (int x, No i, No pai) {
+            if (i == null) {
+                if (x < pai.elemento) pai.esq = new No (x);
+                else pai.dir = new No (x);
+            }
+            else if (x < i.elemento) inserirPai(x, i.esq, i);
+            else if (x > i.elemento) inserirPai(x, i.dir, i);
+            else throw new RuntimeException ("Erro!");
+            }
         }
         boolean pesquisar (int x) {
 
@@ -37,7 +55,7 @@ public class arvoreBin {
 
         }
         void remover (int x) {
-            
+
         }
     }
 }
