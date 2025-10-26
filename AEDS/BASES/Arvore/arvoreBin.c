9ab2487405398b5cb73fc766c49cb6d86fc16b00
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-10-26 10:31:30 -0300

    att

--- AEDS/BASES/Arvore/arvoreBin.c
+++ AEDS/BASES/Arvore/arvoreBin.c
@@ -14,50 +14,76 @@ No *newNo (int x) {
     return novo;
 }
 
-typedef struct Arvore {
-    No *raiz;
-} Arvore;
+No *raiz;
 
-Arvore* newArvore () {
-    Arvore *nova = (Arvore*)malloc(sizeof(Arvore));
-    nova->raiz = NULL;
-    return nova;
+void Arvore () {
+    raiz = NULL;
 }
 
-No* inserirRef (int x, No *i) {
+No* inserirRefArvore (int x, No *i) {
     if (i == NULL) i = newNo(x);
-    else if (x < i->elemento) i->esq = inserirRef(x, i->esq);
-    else if (x > i->elemento) i->dir = inserirRef(x, i->dir);
+    else if (x < i->elemento) i->esq = inserirRefArvore(x, i->esq);
+    else if (x > i->elemento) i->dir = inserirRefArvore(x, i->dir);
     else exit(1);
     return i;
 }
-void inserirRefArvore (Arvore *a, int x) {
-    a->raiz = inserirRef(x, a->raiz);
+void inserirRef (int x) {
+    raiz = inserirRefArvore(x, raiz);
 }
 
-void inserirPai (Arvore *a, int x, No *i, No *pai) {
+void inserirPaiArvore (int x, No *i, No *pai) {
     if (i == NULL) {
         if (x < pai->elemento) pai->esq = newNo(x);
         else pai->dir = newNo(x);
-    } else if (x < i->elemento) inserirPai(a, x, i->esq, i);
-    else if (x > i->elemento) inserirPai(a, x, i->dir, i);
+    } else if (x < i->elemento) inserirPaiArvore(x, i->esq, i);
+    else if (x > i->elemento) inserirPaiArvore(x, i->dir, i);
     else exit(1);
 }
-void inserirPaiArvore (Arvore *a, int x) {
-    if (a->raiz == NULL) a->raiz = newNo(x);
-    else if (x < a->raiz->elemento) inserirPai(a, x, a->raiz->esq, a->raiz);
-    else if (x > a->raiz->elemento) inserirPai(a, x, a->raiz->dir, a->raiz);
+void inserirPai (int x) {
+    if (raiz == NULL) raiz = newNo(x);
+    else if (x < raiz->elemento) inserirPaiArvore(x, raiz->esq, raiz);
+    else if (x > raiz->elemento) inserirPaiArvore(x, raiz->dir, raiz);
     else exit(1);
 }
 
-bool pesquisar (Arvore *a, int x) {
-    return pesquisarNo(a->raiz, x);
-}
 bool pesquisarNo (No *i, int x) {
     if (i == NULL) return false;
     else if (x == i->elemento) return true;
     else if (x < i->elemento) return pesquisarNo(i->esq, x);
     else return pesquisarNo(i->dir, x);
+}
+bool pesquisar (int x) {
+    return pesquisarNo(raiz, x);
+}
+
+No* removerNo (int x, No *i) {
+    if (i == NULL) exit(1);
+    else if (x < i->elemento) i->esq = removerNo(x, i->esq);
+    else if (x > i->elemento) i->dir = removerNo(x, i->dir);
+    else if (i->dir == NULL) i = i->esq;
+    else if (i->esq == NULL) i = i->dir;
+    else i->esq = maiorEsq(i, i->esq);
+    return i;
+}
+void remover (int x) {
+    raiz = removerNo(x, raiz);
+}
+
+No* maiorEsq (No *i, No *j) {
+    if (j->dir == NULL) {
+        i->elemento = j->elemento;
+        j = j->esq;
+    }
+    else j->dir = maiorEsq(i, j->dir);
+    return j;
+}
+No* menorDir (No *i, No *j) {
+    if (j->esq == NULL) {
+        i->elemento = j->elemento;
+        j = j->dir;
+    }
+    else j->esq = menorDir(i, j->esq);
+    return j;
 }
 
 void caminharCentral (No *i) {
@@ -82,36 +108,6 @@ void caminharPos (No *i) {
     }
 }
 
-void remover (Arvore *a, int x) {
-    a->raiz = removerNo(x, a->raiz);
-}
-No* removerNo (int x, No *i) {
-    if (i == NULL) exit(1);
-    else if (x < i->elemento) i->esq = removerNo(x, i->esq);
-    else if (x > i->elemento) i->dir = removerNo(x, i->dir);
-    else if (i->dir == NULL) i = i->esq;
-    else if (i->esq == NULL) i = i->dir;
-    else i->esq = maiorEsq(i, i->esq);
-    return i;
-}
-
-No* maiorEsq (No *i, No *j) {
-    if (j->dir == NULL) {
-        i->elemento = j->elemento;
-        j = j->esq;
-    }
-    else j->dir = maiorEsq(i, j->dir);
-    return j;
-}
-No* menorDir (No *i, No *j) {
-    if (j->esq == NULL) {
-        i->elemento = j->elemento;
-        j = j->dir;
-    }
-    else j->esq = menorDir(i, j->esq);
-    return j;
-}
-
 int getAlturaRec (No *i) {
     if (i == NULL) return 0;
     else {
@@ -121,16 +117,16 @@ int getAlturaRec (No *i) {
     }
 }
 
-int getMaior (Arvore *a) {
-    if (a->raiz == NULL) exit(1);
+int getMaior () {
+    if (raiz == NULL) exit(1);
     No *i;
-    for (i = a->raiz; i->dir != NULL; i = i->dir);
+    for (i = raiz; i->dir != NULL; i = i->dir);
     return i->elemento;
 }
-int getMenor (Arvore *a) {
-    if (a->raiz == NULL) exit(1);
+int getMenor () {
+    if (raiz == NULL) exit(1);
     No *i;
-    for (i = a->raiz; i->esq != NULL; i = i->esq);
+    for (i = raiz; i->esq != NULL; i = i->esq);
     return i->elemento;
 }
 
@@ -139,12 +135,9 @@ int somarRec (No *i) {
     return i->elemento + somarRec(i->esq) + somarRec(i->dir);
 }
 
-bool igual (Arvore *a, Arvore *b) {
-    return igualNo(a->raiz, b->raiz);
-}
-bool igualNo (No *a, No *b) {
+bool igual (No *a, No *b) {
     if (a == NULL && b == NULL) return true;
     else if (a == NULL || b == NULL) return false;
     else if (a->elemento != b->elemento) return false;
-    else return igualNo(a->esq, b->esq) && igualNo(a->dir, b->dir);
+    else return igual(a->esq, b->esq) && igual(a->dir, b->dir);
 }
