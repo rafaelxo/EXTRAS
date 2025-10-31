65d1749228f6af61f283d80990a78124576bee9e
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-10-31 19:21:05 -0300

    att

--- AEDS/BASES/Ordenacao/mergesort.c
+++ AEDS/BASES/Ordenacao/mergesort.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 
 void intercalar (int vet[], int esq, int meio, int dir) {
-    int n1 = meio - esq + 1, n2 = dir - meio;
-    int a1[n1], a2[n2];
-    for (int i = 0; i < n1; i++) a1[i] = vet[esq + i];
-    for (int i = 0; i < n2; i++) a2[i] = vet[meio + i + 1];
-    int i = 0, j = 0, k = esq;
-    while (i < n1 && j < n2) {
-        if (a1[i] <= a2[j]) vet[k++] = a1[i++];
-        else vet[k++] = a2[j++];
+    int nEsq = meio - esq + 1, nDir = dir - meio;
+    int *vetEsq = (int*)malloc((nEsq + 1)*sizeof(int));
+    int *vetDir = (int*)malloc((nDir + 1)*sizeof(int));
+    vetEsq[nEsq] = vetDir[nDir] = 2147483647;
+    for (int i = 0; i < nEsq; i++) vetEsq[i] = vet[esq + i];
+    for (int j = 0; j < nDir; j++) vetDir[j] = vet[meio + 1 + j];
+    for (int j = 0, k = 0, i = esq; i <= dir; i++) {
+        if (vetEsq[j] <= vetDir[k]) vet[i] = vetEsq[j++];
+        else vet[i] = vetDir[k++];
     }
-    while (i < n1) vet[k++] = a1[i++];
-    while (j < n2) vet[k++] = a2[j++];
 }
 
-void mergesort (int vet[], int esq, int dir) {
+void merge (int vet[], int esq, int dir) {
     if (esq < dir) {
         int meio = (esq + dir) / 2;
-        mergesort(vet, esq, meio);
-        mergesort(vet, meio + 1, dir);
+        merge(vet, esq, meio);
+        merge(vet, meio + 1, dir);
         intercalar(vet, esq, meio, dir);
     }
 }
--- AEDS/BASES/Ordenacao/mergesort.java
+++ AEDS/BASES/Ordenacao/mergesort.java
@@ -1,3 +1,22 @@
 public class mergesort {
-    
+    public static void intercalar (int vet[], int esq, int meio, int dir) {
+        int nEsq = meio - esq + 1, nDir = dir - meio;
+        int[] vetEsq = new int[nEsq + 1];
+        int[] vetDir = new int[nDir + 1];
+        vetEsq[nEsq] = vetDir[nDir] = 0x7FFFFFFF;
+        for (int i = 0; i < nEsq; i++) vetEsq[i] = vet[esq + i];
+        for (int j = 0; j < nDir; j++) vetDir[j] = vet[meio + 1 + j];
+        for (int j = 0, k = 0, i = esq; i <= dir; i++) {
+            if (vetEsq[j] <= vetDir[k]) vet[i] = vetEsq[j++];
+            else vet[i] = vetDir[k++];
+        }
+    }
+    public static void merge (int vet[], int esq, int dir) {
+        if (esq < dir) {
+            int meio = (esq + dir) / 2;
+            merge(vet, esq, meio);
+            merge(vet, meio + 1, dir);
+            intercalar(vet, esq, meio, dir);
+        }
+    }
 }
