d3e336f507ce64259490a663219b35cd292fe7a4
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-10-31 15:14:27 -0300

    att

--- AEDS/BASES/Ordenacao/buble.java
+++ AEDS/BASES/Ordenacao/buble.java
@@ -1,12 +1,10 @@
-import java.util.*;
-
 public class buble {
     public static void swap (int array[], int i, int j) {
         int aux = array[i];
         array[i] = array[j];
         array[j] = aux;
     }
-    
+
     public static void bolha (int array[], int n) {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
