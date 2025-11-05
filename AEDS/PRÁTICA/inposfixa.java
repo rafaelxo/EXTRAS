069c17e79c1f906c24c64102a6db0bd6ce44b446
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-11-05 11:30:44 -0300

    att

---
+++ AEDS/PRÁTICA/inposfixa.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class inposfixa {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static String transformar (String str) {
+        String nova = "";
+
+        return nova;
+    }
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            str = transformar(str);
+            System.out.println(str);
+        }
+    }
+}
