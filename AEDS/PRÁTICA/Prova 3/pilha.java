55e2415274dae902be3769f473d60535ce48dca4
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-12-02 09:17:47 -0300

    att

--- AEDS/PRÁTICA/Prova 3/pilha.java
+++ AEDS/PRÁTICA/Prova 3/pilha.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class pilha {
     public static Scanner sc = new Scanner (System.in);
@@ -31,21 +31,45 @@ public class pilha {
         }
     }
 
-    public static boolean resolver (Pilha p1, Pilha p2, Pilha p3) {
-        
+    private static boolean jogar (Pilha p1, Pilha p2, Pilha p3, int soma) {
+        if (soma > 0 && soma % 3 == 0) return true;
+        if (p1.topo != null) {
+            int valor = p1.pop();
+            if (jogar(p1, p2, p3, soma + valor)) {
+                p1.push(valor);
+                return true;
+            }
+            p1.push(valor);
+        }
+        if (p2.topo != null) {
+            int valor = p2.pop();
+            if (jogar(p1, p2, p3, soma + valor)) {
+                p2.push(valor);
+                return true;
+            }
+            p2.push(valor);
+        }
+        if (p3.topo != null) {
+            int valor = p3.pop();
+            if (jogar(p1, p2, p3, soma + valor)) {
+                p3.push(valor);
+                return true;
+            }
+            p3.push(valor);
+        }
         return false;
     }
 
     public static void main (String[] args) {
-        while (sc.hasNextInt()) {
-            int n = sc.nextInt();
+        int n = sc.nextInt();
+        while (n != 0) {
             Pilha p1 = new Pilha (), p2 = new Pilha (), p3 = new Pilha ();
             for (int i = 0; i < n; i++) {
                 int valor = sc.nextInt(); p1.push (valor);
                 valor = sc.nextInt(); p2.push (valor);
                 valor = sc.nextInt(); p3.push (valor);
             }
-            System.out.println(resolver(p1, p2, p3) ? "1" : "0");
+            System.out.println(jogar(p1, p2, p3, 0) ? "1" : "0");
             n = sc.nextInt();
         }
     }
