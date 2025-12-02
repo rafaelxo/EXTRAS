24ff653d5122b37e26fe5be42feda87d5d20d537
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-12-02 17:10:46 -0300

    att

---
+++ AEDS/PRÁTICA/Prova 3/anota.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int *vet, int i, int j) {
+    int temp = vet[i];
+    vet[i] = vet[j];
+    vet[j] = temp;
+}
+
+void ordenacao (int *vet, int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (vet[j] > vet[j+1]) swap(vet, j, j+1);
+        }
+    }
+}
+
+int main () {
+    int n, k;
+    while (scanf("%d %d", &n, &k) != EOF) {
+        int vet[n];
+        for (int i = 0; i < n; i++) scanf("%d", &vet[i]);
+        ordenacao(vet, n);
+        int soma = 0;
+        for (int i = n - 1; i >= n - k; i--) soma += vet[i];
+        printf("%d\n", soma);
+    }
+}
